36e66505b8d5994d3ac159b776df5455ad01e1b1
Marijn Koesen <github@koesen.nl>
2014-03-05 10:09:15 +0100

    Fixed bug after config layout change

--- src/codegenerator/CodeGenerator.php
+++ src/codegenerator/CodeGenerator.php
@@ -1,6 +1,7 @@
 <?php
 namespace codegenerator;
 
+use codegenerator\generator\AbstractGenerator;
 use codegenerator\model\ClassEntity;
 
 class CodeGenerator
@@ -16,10 +17,12 @@ class CodeGenerator
     {
         $html = '<ul class="nav nav-tabs">';
 
-        foreach($modules as $index => $module) {
-            $obj = new $module();
+        $index = 0;
+        foreach($modules as $module => $enabled) {
+            $obj = $this->createGenerator($module);
             $class = $index == 0 ? ' class="active"' : '';
             $html .= '<li' . $class . '><a href="#' . $obj->getName() . '" data-toggle="tab">' . $obj->getName() . '</a></li>';
+            $index++;
         }
         $html .= '</ul>';
         return $html;
@@ -31,17 +34,35 @@ class CodeGenerator
 
         $html = '<div id="myTabContent" class="tab-content">';
 
-        foreach($modules as $index => $module) {
-            $generator = new $module();
+        $index = 0;
+        foreach($modules as $module => $enabled) {
+            $generator = $this->createGenerator($module);
             $divClass = $index == 0 ? ' active' : '';
 
             $html .= '
                 <div class="tab-pane fade in' . $divClass . '" id="' . $generator->getName() . '">
                     ' . $generator->generateCode($class) . '
                 </div>';
+
+            $index++;
         }
 
         $html .= '</div>';
         return $html;
+    }
+
+    protected function createGenerator($name)
+    {
+        if (!class_exists($name)) {
+            throw new \Exception("Cannot find codegenerator class '" . $name . "', make sure the class is available and namespace exists");
+        }
+
+        $generator = new $name();
+
+        if (!$generator instanceof AbstractGenerator) {
+            throw new \Exception("Class '" . $name . "' is not derived from AbstractGenerator");
+        }
+
+        return $generator;
     }
 }
